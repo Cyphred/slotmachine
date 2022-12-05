7e385c17513a490f48a5d5ee4f93103f9dfd2b9f
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2022-12-06 00:50:16 +0800

    Added data visualization
    - Modified prize amounts
    - Renamed isHit to isMatch
    - Added limit for hitting the jackpot
    - Added tracker for net gain/loss
    - Added bar and line graphs

--- slotmachine.py
+++ slotmachine.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 REEL = ('7', 'Bar', 'Cherry', 'Apple', 'Banana', 'Grapes', 'Grapes', 'Banana', 'Apple', 'Banana', 'Grapes', 'Banana', 'Apple')
 
@@ -15,6 +16,9 @@ tracker = {
     "Lose": 0,
 }
 
+# Tracks the history of wealth/credits relative to the number of turns
+wealth_history = []
+
 def pull():
     result_list = []
     for i in range(3):
@@ -26,14 +30,14 @@ def tableCheck(result):
     global tracker
     global bet_cost
 
-    if not isHit(result):
+    if not isMatch(result):
         tracker['Lose'] += 1
         return -1 * bet_cost
         
     value = result[0]
     if value == '7':
         tracker['7'] += 1
-        return 120
+        return 500
     elif value == 'Bar':
         tracker['Bar'] += 1
         return 30
@@ -52,7 +56,7 @@ def tableCheck(result):
         tracker['Lose'] += 1
         return -1 * bet_cost
 
-def isHit(result):
+def isMatch(result):
     if result[0] == result[1] and result[0] == result[2]:
         return True
     return False
@@ -62,14 +66,19 @@ def main():
     starting_credits = int(input('Enter the amount of player coins to start with: '))
     player_credits = starting_credits
     
-    jackpot_limit = int(input('Stop after n hitting the jackpot n times (Enter 0 for no limit): '))
+    jackpot_limit = 0
+    try:
+        jackpot_limit = int(input('Stop after n hitting the jackpot n times (Enter 0 for no limit): '))
+    except:
+        jackpot_limit = 0
     jackpot_counter = 0
 
     while player_credits > 0:
         pull_count += 1
         result = pull()
         player_credits = player_credits + tableCheck(result)
-        if isHit(result) and result[0] == '7':
+        wealth_history.append(player_credits)
+        if isMatch(result) and result[0] == '7':
             jackpot_counter += 1
             if jackpot_limit > 0 and jackpot_counter == jackpot_limit:
                 print(f'Jackpot limit of {jackpot_limit} reached.')
@@ -78,7 +87,27 @@ def main():
     global tracker
     print(f'Pulled {pull_count} times.')
     print(f'Game ended with {player_credits} credits left.')
+    if player_credits == starting_credits:
+        print('Player broke even.')
+    elif player_credits < starting_credits:
+        print(f'Player at net loss of -{starting_credits - player_credits}')
+    elif player_credits > starting_credits:
+        print(f'Player at net profit of +{player_credits - starting_credits}')
     print(tracker)
+    
+    figure, axis = plt.subplots(2, 1)
+
+    axis[0].plot(wealth_history)
+    axis[0].set_title('Credit History')
+
+    bar_labels = list(tracker.keys())
+    bar_values = list(tracker.values())
+    axis[1].bar(bar_labels, bar_values)
+    for i in range(len(bar_labels)):
+        axis[1].text(i,bar_values[i],bar_values[i], ha='center')
+    axis[1].set_title('Likelihood of a match/loss')
+    
+    plt.show()
 
 if __name__ == "__main__":
     main()
